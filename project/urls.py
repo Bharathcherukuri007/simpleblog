820ced5b9371173eedef141949b7fc909899fc37
Bharathcherukuri007 <Bharathcherukuri007@github.com>
2021-05-30 15:21:53 +0530

    first commit

---
+++ project/urls.py
@@ -0,0 +1,12 @@
+from . import views
+from .views import *
+from django.urls import path
+
+urlpatterns=[
+    path('',register,name="register"),
+    path('login',login,name="login"),
+    path('Post',Post,name="Post"),
+    path('delete/<int:id>',delete,name="delete"),
+    path("show",show,name="show"),
+
+]
